7d55726ef03019d824c0cc0c57bfe5920864ccf5
npdarsini <npdarsinius15@gmail.com>
2016-11-20 23:27:11 -0600

    Kafka Part

--- Source/Phase-3/FeatureExtraction/target/classes/SendFeaturesKafka.java
+++ Source/Phase-3/FeatureExtraction/target/classes/SendFeaturesKafka.java
@@ -1,41 +1,44 @@
-import com.migcomponents.migbase64.Base64;
-import com.xuggle.xuggler.IContainer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-//import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import kafka.producer.ProducerConfig;
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
 
-import java.io.File;
 import java.util.Properties;
+
+//import org.apache.kafka.clients.producer.Producer;
 
 /**
  * Created by harsha on 11/17/16.
  */
 public class SendFeaturesKafka {
 
+
     public static void main(String[] args) {
-        String topic = "featuresToSpark";
+
+        String topic = "priya";
         Producer<Integer, String> producer;
         Properties properties = new Properties();
-        properties.put("metadata.broker.list", "127.0.0.1:9092");
+        properties.put("metadata.broker.list", "192.168.0.3:9092");
         properties.put("serializer.class", "kafka.serializer.StringEncoder");
         properties.put("request.required.acks", "1");
-        properties.put("message.max.bytes", "10000000");
+        //properties.put("message.max.bytes", "10000000");
+
+        ProducerConfig config = new ProducerConfig(properties);
+
         producer = new Producer<Integer, String>(new ProducerConfig(properties));
         System.out.println("Key Frames Generated");
 //        File folder = new File("output/mainframes");
 //        File[] listOfFiles = folder.listFiles();
         String fileName = "output/features.txt";
         EncodeData ed = new EncodeData();
-        String encStr = ed.encodeToString(fileName);
+        byte [] encStr = ed.encodeToString(fileName);
+        System.out.println("encStr: " + encStr);
         String fileNameLength = Integer.toString(fileName.length());
         System.out.println("Simple Filename Length: " + fileNameLength);
 //            msg = "*@#Harsha*@#" +  " " + fileName + " " + msg + "#@*Sri#@*";
-        encStr = fileName + " " + encStr;
-        System.out.println("Encoded String: " + encStr.length());
-        KeyedMessage<Integer, String> data = new KeyedMessage<Integer, String>(topic, encStr);//Encoding the Video
+        String encStrFName = fileName + ";" + encStr;
+        System.out.println("Encoded String: " + encStrFName.length());
+        KeyedMessage<Integer, String> data = new KeyedMessage<Integer, String>(topic, encStrFName);//Encoding the Video
+        System.out.println("Data is:"+ data);
         producer.send(data);
         System.out.println("Message Sent");
     }
